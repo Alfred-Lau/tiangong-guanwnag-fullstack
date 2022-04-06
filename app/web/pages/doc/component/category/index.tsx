a607aa163286054e4e97425e97750d01d622bb9a
Alfred-Lau <jian.liu@icloud.com>
2022-04-07 00:23:07 +0800

    feat: add doc component css transition

--- app/web/pages/doc/component/category/index.tsx
+++ app/web/pages/doc/component/category/index.tsx
@@ -1,28 +1,31 @@
-import React from "react";
-import { Tree } from 'antd';
-import { DownOutlined } from '@ant-design/icons';
+import React, { forwardRef } from "react";
+import { Tree } from "antd";
+import { DownOutlined } from "@ant-design/icons";
 import styles from "./index.less";
 
 export type CategoryProps = {
   data: any;
 };
 
-const Category = (props: CategoryProps) => {
-  const { data=[] } = props;
+const Category = forwardRef((props: CategoryProps, ref) => {
+  const { data = [] } = props;
   console.log("data", data);
 
-  const onSelect =  (selectedKeys, info) => {
-    console.log('selected', selectedKeys, info);
+  const onSelect = (selectedKeys, info) => {
+    console.log("selected", selectedKeys, info);
   };
-  return <div className={styles.category}>
-    <Tree
+
+  return (
+    <div className={styles.category} ref={ref}>
+      <Tree
         showLine
         switcherIcon={<DownOutlined />}
-        defaultExpandedKeys={['0-0-0']}
+        defaultExpandedKeys={["0-0-0"]}
         onSelect={onSelect}
         treeData={data}
-    />
-  </div>;
-};
+      />
+    </div>
+  );
+});
 
 export default Category;
